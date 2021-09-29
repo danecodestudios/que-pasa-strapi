adf1810a22d12c65fd40720ef816b4ea3e5a0829
danecodestudios <danecodsas@gmail.com>
2021-09-29 10:50:18 -0500

    deploy

--- config/database.js
+++ config/database.js
@@ -1,36 +1,15 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST"),
-//         srv: env.bool("DATABASE_SRV", true),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME"),
-//         username: env("DATABASE_USERNAME"),
-//         password: env("DATABASE_PASSWORD"),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", true),
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.hfnlt.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "que-pasa"),
-        username: env("DATABASE_USERNAME", "admin"),
-        password: env("DATABASE_PASSWORD", "Danecod2021"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
@@ -39,3 +18,24 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         host: env("DATABASE_HOST", "cluster0.hfnlt.mongodb.net"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", "que-pasa"),
+//         username: env("DATABASE_USERNAME", "admin"),
+//         password: env("DATABASE_PASSWORD", "Danecod2021"),
+//       },
+//       options: {
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
+//       },
+//     },
+//   },
+// });
